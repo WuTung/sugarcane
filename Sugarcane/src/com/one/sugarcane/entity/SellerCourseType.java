59795a7c9fe025247fc4338da8ffdc2ab05f1716
吴桐 <478235329@qq.com>
2018-04-26 08:38:33 +0800

    add project

---
+++ Sugarcane/src/com/one/sugarcane/entity/SellerCourseType.java
@@ -0,0 +1,58 @@
+package com.one.sugarcane.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+@Entity
+@Table(name="SELLERCOURSETYPE")
+public class SellerCourseType {
+	
+	private Integer sellerCourseTypeID;
+	private String courseTypeName2;
+	private int hidden;
+	private Set<Course>course = new HashSet<Course>();
+	
+	@Id
+	@GeneratedValue(generator="a")
+	@GenericGenerator(name="a",strategy="identity")
+	public Integer getSellerCourseTypeID() {
+		return sellerCourseTypeID;
+	}
+	public void setSellerCourseTypeID(Integer sellerCourseTypeID) {
+		this.sellerCourseTypeID = sellerCourseTypeID;
+	}
+	public String getCourseTypeName2() {
+		return courseTypeName2;
+	}
+	public void setCourseTypeName2(String courseTypeName2) {
+		this.courseTypeName2 = courseTypeName2;
+	}
+	public int getHidden() {
+		return hidden;
+	}
+	public void setHidden(int hidden) {
+		this.hidden = hidden;
+	}
+	@OneToMany(mappedBy="sellerCourseType",targetEntity=Course.class,cascade=CascadeType.MERGE)
+	public Set<Course> getCourse() {
+		return course;
+	}
+	public void setCourse(Set<Course> course) {
+		this.course = course;
+	}
+	public SellerCourseType(String courseTypeName2, int hidden, Set<Course> course) {
+		super();
+		this.courseTypeName2 = courseTypeName2;
+		this.hidden = hidden;
+		this.course = course;
+	}
+	
+}
