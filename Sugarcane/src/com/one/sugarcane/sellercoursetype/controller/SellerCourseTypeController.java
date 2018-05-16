5df67c89c654b8302abc1ef686afd0896680e2ce
MeccaMe <1010870945@qq.co,m>
2018-05-16 08:36:09 +0800

    培训机构分类

--- Sugarcane/src/com/one/sugarcane/sellercoursetype/controller/SellerCourseTypeController.java
+++ Sugarcane/src/com/one/sugarcane/sellercoursetype/controller/SellerCourseTypeController.java
@@ -5,14 +5,110 @@
  */
 package com.one.sugarcane.sellercoursetype.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.one.sugarcane.course.service.CourseServiceImpl;
+import com.one.sugarcane.entity.Course;
+import com.one.sugarcane.entity.PublicCourseType;
+import com.one.sugarcane.entity.SellerCourseType;
+import com.one.sugarcane.sellercoursetype.service.SellerCourseTypeServiceImpl;
 import com.one.sugarcane.test.service.TestService;
-
+@Controller
+@RequestMapping("sellerCourseType")
 public class SellerCourseTypeController {
+	@Resource
+	private SellerCourseTypeServiceImpl sellerCourseTypeServiceImpl;
+	private CourseServiceImpl courseServiceImpl;
 
+	/**
+	 * 列表查询
+	 * @author 王孜润
+	 * @date 2018/5/1
+	 * **/
+	@RequestMapping("/sellerCourseType")
+//	private String sellerCourseType(Model model) {
+	private String sellerCourseType(Model model,HttpServletResponse response,@RequestParam(value="pageNum",defaultValue="1")Integer page,HttpServletRequest request) {
+//		List<SellerCourseType> list = sellerCourseTypeServiceImpl.findAll();
+		List<SellerCourseType> list = this.sellerCourseTypeServiceImpl.findAll(page);
+		model.addAttribute("sellerCourseType",list);
+		List<PublicCourseType> list1 = sellerCourseTypeServiceImpl.findTypeAll();
+		model.addAttribute("publicCourseType",list1);
+		for(int i=0;i<list1.size();i++) {
+			System.out.println(list1.get(i).getPublicTypeName());
+		}
+		
+		//分页
+		int pageCount = this.sellerCourseTypeServiceImpl.getPageCount();
+		model.addAttribute("pageCount",pageCount);
+		int pageNum = 1;
+		model.addAttribute("pageNum",pageNum);
+		if(pageNum==0 || pageNum<0) {
+			model.addAttribute("pageNum",1);
+		}else {
+			model.addAttribute("pageNum",pageNum);
+		}
+		return "organization/manageClassify";
+	}
+	/**
+	 * 添加
+	 * @author 王孜润
+	 * @date 2018/5/7
+	 * **/
+	@RequestMapping("/addCourseType")
+	private String addCourseType(Model model,HttpServletRequest request,HttpServletResponse response) {
+		Course course = new Course();
+		String newCourseName = request.getParameter("courseName");
+		String newCTypeName = request.getParameter("publicTypeName");
+		List<PublicCourseType> pList = sellerCourseTypeServiceImpl.findByName(newCTypeName);
+		PublicCourseType pcype = pList.get(0);
+//		Integer pctId = pcype.getPublicTypeId();
+		course.setCourseName(newCourseName);
+		course.setPublicCourseType(pcype);
+		boolean result = sellerCourseTypeServiceImpl.addNewCourseType(course);
+		System.out.println("走到这里了");
+		System.out.println(result);
+		if(result) {
+			System.out.println("成功的走到这里了");
+			return "redirect:sellerCourseType";
+		}else {
+			model.addAttribute("errormsg", "添加失败");
+			return "organization/manageClassify";
+		}
+	}
+	/**
+	 * 删除
+	 * @author 王孜润
+	 * @date 2018/5/1
+	 * **/
+	@RequestMapping("/deleteCourseType")
+	private String deleteCourseType(Model model,HttpServletRequest request) {
+		String id = request.getParameter("id");
+		int cId = Integer.valueOf(id);
+		boolean result = sellerCourseTypeServiceImpl.deleteCourseType(cId);
+		if(result) {
+			return "redirect:sellerCourseType";
+		}else {
+			model.addAttribute("errormsg","添加失败");
+			return "redirect:sellerCourseType";
+		}
+	}
+	/**
+	 * 更新课程
+	 * @author 王孜润
+	 * @date 2018/5/15
+	 * **/
+//	@RequestMapping("/updateCourseType")
+//	private String updateCourseType(Model model) {
+//		
+//	}
 }
